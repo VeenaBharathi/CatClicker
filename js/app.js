d75f5cf50c7e1f0c40925ac89d0da97a4e61fd49
Veena Bharathi Akilla <veenabharathi93@gmail.com>
2018-11-12 17:23:04 +0530

    Final Changes

--- js/app.js
+++ js/app.js
@@ -1,46 +1,62 @@
-var count1 = $('#count1');
-var count2 = $('#count2');
-var kit = $('.kit');
-var cheu = $('.cheu');
-var i=0,j =0;
+var catObj = [
+{name: "kit", ref: "http://www.petwebsite.co.uk/media/k2/items/cache/0548677e6432786dd8df61eb3aaec139_L.jpg"},
+{name: "cheu", ref: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"},
+{name: "meow", ref: "https://cdn.britannica.com/47/158247-131-9FB76EAE.jpg"},
+{name: "pit", ref: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkhnSdMgyv5wkZIPTD-CNmy_D9ibkgvdRqMyvXhjEmLjNVgFXiFg"},
+{name: "charl", ref: "https://assets.rbl.ms/14419411/980x.jpg"}
+];
+
+/*for (var cat in catObj){
+
+$('.name').click(function(catcopy){
+	function(){
+			$('.name').text(cat.name);
+			$('.cat').text(cat.ref);
+	}
+
+})(cat);*/
 
 
 
-var cats = { name: "meou", 
-ref: "http://www.petwebsite.co.uk/media/k2/items/cache/0548677e6432786dd8df61eb3aaec139_L.jpg"
-} ;
+var i=0;
+$('.thumbnail').click(function(){
 
-// setting act names
-$('.name1').html("<strong>KIT</strong>");
-$('.name2').html("<strong>CHEW</strong>");
+	console.log("clicked on cat: " + this.textContent);
+	var currCat = this.textContent;
+	
+	for(var cat of catObj){
 
-kit.click( function(){
-	i += 1;
-	count1.html(i);
+		if((cat.name).toUpperCase() === currCat){
+			$('.name').text(cat.name);
+			$('.cat').attr('src',(cat.ref));
+			$('.cat').attr('alt',(cat.name));
+			break;
+		}
+	}
 });
 
-cheu.click( function(){
-	j += 1;
-	count2.html(j);
-});
+	
 
-var catdiv = document.createElement("div");
-catdiv.classList.add("cat");
-console.log(catdiv);
-$('.catsgroup').append(catdiv);
+		$('.cat').click((function(){
 
-var namediv = document.createElement("div");
-namediv.classList.add("name");
-console.log(namediv);
-$('.cat').append(namediv);
+			console.log("in lsitsner for cat");
+			return function(){
+				
+				let divele =  this.alt.toLowerCase() ;
+				
+				let count = $('.' + divele).find('#count');
+				i = parseInt(count.text()) + 1;
+				console.log(count.text());
+				// console.log(j);
+				count.html(i);
+			}
 
-var catimg = document.createElement("img");
-catimg.classList.add(cats.name);
-// $('\'.'+cats.name+'\'').src(cats.ref);
-// $('.meow').attr('src',"\"" + cats.ref + "\"");
-$('.meow').attr('alt','meow');
-$('.meow').src = "\"" + cats.ref + "\"";
-console.log($('.meow'));
-$('.cat').append(catimg);
+		})());
+
+
+
+
+
+
 
 
