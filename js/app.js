ac38c83801d91e0accad4dcfdf5ad0b9d9527d2e
Veena Bharathi Akilla <veenabharathi93@gmail.com>
2018-11-12 13:23:36 +0530

    Added dynamic  cat to cats group in cat area

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ var cheu = $('.cheu');
 var i=0,j =0;
 
 
+
+var cats = { name: "meou", 
+src: "http://www.petwebsite.co.uk/media/k2/items/cache/0548677e6432786dd8df61eb3aaec139_L.jpg"
+} ;
+
 // setting act names
 $('.name1').html("<strong>KIT</strong>");
 $('.name2').html("<strong>CHEW</strong>");
@@ -18,3 +23,20 @@ cheu.click( function(){
 	j += 1;
 	count2.html(j);
 });
+
+var catdiv = document.createElement("div");
+catdiv.classList.add("cat");
+console.log(catdiv);
+$('.catsgroup').append(catdiv);
+
+var namediv = document.createElement("div");
+namediv.classList.add("name");
+console.log(namediv);
+$('.cat').append(namediv);
+
+var catimg = document.createElement("img");
+catimg.classList.add(cats.name);
+console.log(catimg);
+$('.cat').append(catimg);
+
+
