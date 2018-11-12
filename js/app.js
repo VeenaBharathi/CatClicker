cf61f8287e6e14f6dc349ebab0b3e9039aa285ab
Veena Bharathi Akilla <veenabharathi93@gmail.com>
2018-11-12 12:19:42 +0530

    Changes for Cat clicker duo

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
-var count = $('#count');
+var count1 = $('#count1');
+var count2 = $('#count2');
 var kit = $('.kit');
 var cheu = $('.cheu');
-var i =0;
+var i=0,j =0;
+
+// setting act names
+$('.name1').html("<strong>KIT</strong>");
+$('.name2').html("<strong>CHEW</strong>");
 
 kit.click( function(){
 	i += 1;
-	count.html(i);
+	count1.html(i);
 });
 
 cheu.click( function(){
-	i += 1;
-	count.html(i);
+	j += 1;
+	count2.html(j);
 });
